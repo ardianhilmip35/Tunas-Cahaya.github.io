1b9a20c56f090f8c63950b2257f0e6e2c6ed905f
Ilham_Nugraha <80625504+M-Ilham-Nugraha-Syahputra@users.noreply.github.com>
2021-12-01 21:30:43 +0700

    Add files via upload

---
+++ Mobile/About/app/src/main/java/com/ilham/about/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ilham.about;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
