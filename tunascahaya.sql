7cc54c95f2b5e930c6f077840fda99a6651c32e8
ardianhilmip35 <ardianhil86@gmail.com>
2021-12-01 08:56:13 +0700

    master

--- tunascahaya.sql
+++ tunascahaya.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 24, 2021 at 02:53 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.2
+-- Waktu pembuatan: 01 Des 2021 pada 02.04
+-- Versi server: 10.4.21-MariaDB
+-- Versi PHP: 7.4.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,23 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_jabatan`
+-- Struktur dari tabel `tb_detailpemesanan`
+--
+
+CREATE TABLE `tb_detailpemesanan` (
+  `id_pemesanan` int(3) NOT NULL,
+  `nama_bangunan` varchar(30) NOT NULL,
+  `total_harga` int(10) NOT NULL,
+  `cicilan` int(2) NOT NULL,
+  `hrg_cicilan` int(10) NOT NULL,
+  `tanggal_pembayaran` date NOT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_jabatan`
 --
 
 CREATE TABLE `tb_jabatan` (
@@ -35,18 +51,7 @@ CREATE TABLE `tb_jabatan` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_jenisbiaya`
---
-
-CREATE TABLE `tb_jenisbiaya` (
-  `id_biaya` varchar(1) NOT NULL,
-  `jenis_biaya` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_karyawan`
+-- Struktur dari tabel `tb_karyawan`
 --
 
 CREATE TABLE `tb_karyawan` (
@@ -62,49 +67,36 @@ CREATE TABLE `tb_karyawan` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_katalog`
+-- Struktur dari tabel `tb_katalog`
 --
 
 CREATE TABLE `tb_katalog` (
   `id_katalog` varchar(5) NOT NULL,
   `nama_bangunan` varchar(30) NOT NULL,
-  `id_kategori` varchar(5) NOT NULL,
-  `gambar` varchar(100) NOT NULL,
+  `Kategori` enum('Masjid','Sekolah','Mall','Perumahan') NOT NULL,
+  `gambar` blob NOT NULL,
   `deskripsi` varchar(50) NOT NULL,
-  `alamat` varchar(100) NOT NULL
+  `alamat` varchar(100) NOT NULL,
+  `id_karyawan` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_kategori`
+-- Struktur dari tabel `tb_pemesanan`
 --
 
-CREATE TABLE `tb_kategori` (
-  `id_kategori` varchar(5) NOT NULL,
-  `nama_kategori` varchar(10) NOT NULL
+CREATE TABLE `tb_pemesanan` (
+  `id_pemesanan` int(3) NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `id_katalog` varchar(5) NOT NULL,
+  `tgl_pemesanan` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_keuangan`
---
-
-CREATE TABLE `tb_keuangan` (
-  `no_bukti` int(5) NOT NULL,
-  `uraian` varchar(25) NOT NULL,
-  `penerimaan` int(10) NOT NULL,
-  `pengeluaran` int(10) NOT NULL,
-  `id_biaya` varchar(1) NOT NULL,
-  `saldo` int(10) NOT NULL,
-  `tgl` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_user`
+-- Struktur dari tabel `tb_user`
 --
 
 CREATE TABLE `tb_user` (
@@ -113,7 +105,8 @@ CREATE TABLE `tb_user` (
   `nama` varchar(30) NOT NULL,
   `password` varchar(15) NOT NULL,
   `nomerhp` varchar(13) NOT NULL,
-  `alamat` varchar(50) NOT NULL
+  `alamat` varchar(50) NOT NULL,
+  `id_karyawan` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -121,81 +114,90 @@ CREATE TABLE `tb_user` (
 --
 
 --
--- Indexes for table `tb_jabatan`
+-- Indeks untuk tabel `tb_detailpemesanan`
+--
+ALTER TABLE `tb_detailpemesanan`
+  ADD KEY `id_pemesanan` (`id_pemesanan`);
+
+--
+-- Indeks untuk tabel `tb_jabatan`
 --
 ALTER TABLE `tb_jabatan`
   ADD PRIMARY KEY (`id_jabatan`);
 
 --
--- Indexes for table `tb_jenisbiaya`
---
-ALTER TABLE `tb_jenisbiaya`
-  ADD PRIMARY KEY (`id_biaya`);
-
---
--- Indexes for table `tb_karyawan`
+-- Indeks untuk tabel `tb_karyawan`
 --
 ALTER TABLE `tb_karyawan`
   ADD PRIMARY KEY (`id_karyawan`),
   ADD KEY `id_jabatan` (`id_jabatan`);
 
 --
--- Indexes for table `tb_katalog`
+-- Indeks untuk tabel `tb_katalog`
 --
 ALTER TABLE `tb_katalog`
   ADD PRIMARY KEY (`id_katalog`),
-  ADD KEY `id_kategori` (`id_kategori`);
+  ADD KEY `id_karyawan` (`id_karyawan`);
 
 --
--- Indexes for table `tb_kategori`
+-- Indeks untuk tabel `tb_pemesanan`
 --
-ALTER TABLE `tb_kategori`
-  ADD PRIMARY KEY (`id_kategori`);
+ALTER TABLE `tb_pemesanan`
+  ADD PRIMARY KEY (`id_pemesanan`),
+  ADD KEY `username` (`username`),
+  ADD KEY `id_katalog` (`id_katalog`);
 
 --
--- Indexes for table `tb_keuangan`
---
-ALTER TABLE `tb_keuangan`
-  ADD PRIMARY KEY (`no_bukti`),
-  ADD KEY `id_biaya` (`id_biaya`);
-
---
--- Indexes for table `tb_user`
+-- Indeks untuk tabel `tb_user`
 --
 ALTER TABLE `tb_user`
-  ADD PRIMARY KEY (`username`);
+  ADD PRIMARY KEY (`username`),
+  ADD KEY `id_karyawan` (`id_karyawan`);
 
 --
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tb_keuangan`
---
-ALTER TABLE `tb_keuangan`
-  MODIFY `no_bukti` int(5) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- Constraints for table `tb_karyawan`
+-- AUTO_INCREMENT untuk tabel `tb_pemesanan`
+--
+ALTER TABLE `tb_pemesanan`
+  MODIFY `id_pemesanan` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `tb_detailpemesanan`
+--
+ALTER TABLE `tb_detailpemesanan`
+  ADD CONSTRAINT `tb_detailpemesanan_ibfk_1` FOREIGN KEY (`id_pemesanan`) REFERENCES `tb_pemesanan` (`id_pemesanan`);
+
+--
+-- Ketidakleluasaan untuk tabel `tb_karyawan`
 --
 ALTER TABLE `tb_karyawan`
   ADD CONSTRAINT `tb_karyawan_ibfk_1` FOREIGN KEY (`id_jabatan`) REFERENCES `tb_jabatan` (`id_jabatan`);
 
 --
--- Constraints for table `tb_katalog`
+-- Ketidakleluasaan untuk tabel `tb_katalog`
 --
 ALTER TABLE `tb_katalog`
-  ADD CONSTRAINT `tb_katalog_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `tb_kategori` (`id_kategori`);
+  ADD CONSTRAINT `tb_katalog_ibfk_1` FOREIGN KEY (`id_karyawan`) REFERENCES `tb_karyawan` (`id_karyawan`);
 
 --
--- Constraints for table `tb_keuangan`
+-- Ketidakleluasaan untuk tabel `tb_pemesanan`
 --
-ALTER TABLE `tb_keuangan`
-  ADD CONSTRAINT `tb_keuangan_ibfk_1` FOREIGN KEY (`id_biaya`) REFERENCES `tb_jenisbiaya` (`id_biaya`);
+ALTER TABLE `tb_pemesanan`
+  ADD CONSTRAINT `tb_pemesanan_ibfk_1` FOREIGN KEY (`username`) REFERENCES `tb_user` (`username`),
+  ADD CONSTRAINT `tb_pemesanan_ibfk_2` FOREIGN KEY (`id_katalog`) REFERENCES `tb_katalog` (`id_katalog`);
+
+--
+-- Ketidakleluasaan untuk tabel `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD CONSTRAINT `tb_user_ibfk_1` FOREIGN KEY (`id_karyawan`) REFERENCES `tb_karyawan` (`id_karyawan`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
