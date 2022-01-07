957d20767b5c9f1a4e4e91d54cc78b2abdb9ac98
HilalWahyuPriAditya21 <80748383+HilalWahyuPriAditya21@users.noreply.github.com>
2022-01-07 19:49:51 +0700

    Delete Karyawan directory

--- Karyawan/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require ('../koneksi.php');
-    // $koneksi = new mysqli("localhost", "root", "", "tunascahaya");
-        $id = $_GET['id_karyawan'];
-        mysqli_query($koneksi, "DELETE FROM tb_karyawan WHERE id_karyawan='$id'");
-        header('Location: tampilanawal_karyawan.php');
-?>
